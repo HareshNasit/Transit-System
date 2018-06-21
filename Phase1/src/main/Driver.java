eac506e90284a64f315d5134fecb904a77dbf161
Kara Perrin <kara.perrin@mail.utoronto.ca>
2018-06-21 15:56:33 -0400

    Initial project files.

---
+++ Phase1/src/main/Driver.java
@@ -0,0 +1,11 @@
+package main;
+
+
+/* This class will perform initial setup upon launch. */
+public class Driver {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ Phase1/src/transitNetwork/Line.java
@@ -0,0 +1,5 @@
+package transitNetwork;
+
+public class Line extends Route {
+
+}
