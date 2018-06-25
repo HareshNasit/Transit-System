609edd0972550b99278057a95c8caecc5acff133
Kara Perrin <kara.perrin@mail.utoronto.ca>
2018-06-24 20:30:37 -0400

    TRAN-4 TRAN-9 Users can manage their own cards.

--- phase1/src/user/Admin.java
+++ phase1/src/user/Admin.java
@@ -1,5 +1,20 @@
 package user;
 
+import java.util.ArrayList;
+
 public class Admin {
+  
+  //There should only be one admin instance, so everything here can be static.
+  
+  private static ArrayList<Card> cardList = new ArrayList<Card>();
+  private static ArrayList<User> userList = new ArrayList<User>();
+  
+  public static void addCard(Card card) {
+    cardList.add(card);
+  }
+  
+  public static void addUser(User user) {
+    userList.add(user);
+  }
 
 }
