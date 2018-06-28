03aa15ead89a7dea6726c4abe75e0bf49ade3816
Kara Perrin <kara.perrin@mail.utoronto.ca>
2018-06-27 22:20:43 -0400

    TRAN-15 Began building stop class.

--- phase1/src/transitNetwork/Stop.java
+++ phase1/src/transitNetwork/Stop.java
@@ -1,5 +1,41 @@
 package transitNetwork;
 
+import java.util.ArrayList;
+
 public class Stop {
+  
+  private static int counter = 0;
+  private final int id;
+  private String name;
+  private ArrayList<Stop> adjacentStops = new ArrayList<Stop>();
+  
+  public Stop(String name) {
+    id = counter++;
+    this.name = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public int getID() {
+    return id;
+  }
+  
+  public ArrayList<Stop> getAdjacentStops(){
+    return adjacentStops;
+  }
+  
+  public void addAdjacentStop(Stop stop) {
+    adjacentStops.add(stop);
+  }
+  
+  public void removeAdjacentStop(Stop stop) {
+    if (adjacentStops.contains(stop)) adjacentStops.remove(stop);
+  }
 
 }
