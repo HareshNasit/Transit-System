03f397bd5176e36f42f542250273cc4b9d703f86
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-07-04 21:45:16 -0400

    Made Stop class abstract and added abstract methods tapOn and tapOff and implemented those methods in their subclasses.

--- phase1/src/transitNetwork/BusStop.java
+++ phase1/src/transitNetwork/BusStop.java
@@ -1,8 +1,24 @@
 package transitNetwork;
 
+import user.Card;
+
 public class BusStop extends Stop{
 
     BusStop(String name){
         super(name);
+    }
+
+    //Return true if successful, false otherwise.
+    public boolean tapOn(Card card) {
+        //TODO: implement tap functionality
+        if (card.getBalance() >= 0) {
+            card.charge(2);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean tapOff(Card card) {
+        return true;
     }
 }
