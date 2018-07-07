6e63ce70274f909944ff8700f5312832f9c94954
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-07-07 14:52:00 -0400

    TRAN-45 TRAN-46 Added the method which needs to be implemented and initialized the list connectingStations

--- phase1/src/transitNetwork/Station.java
+++ phase1/src/transitNetwork/Station.java
@@ -9,9 +9,10 @@ public class Station extends Stop{
     private BusStop connectBusStop;
     private ArrayList<Stop> connectingStations;
 
-    public Station(String name, BusStop connectingBusStop){
+    public Station(String name, BusStop connectingBusStop, ArrayList<Stop> connectingStations){
         super(name);
         this.connectBusStop = connectingBusStop;
+        this.connectingStations = connectingStations;
     }
 
     public BusStop getConnectBusStop() {
@@ -25,11 +26,15 @@ public class Station extends Stop{
     }
     
     public boolean tapOff(Route route, Card card) {
-      //TODO: Use route.getDistance() to calculate value of trip
-      //card.charge(route.getDistance() * 0.50)
+      //TODO: Use initialStation.getDistance()*0.5 to calculate value of trip
         card.getCurrentTrip().addStop(this);
         int distance = route.getDistance(card.getCurrentTrip().getInitialStop(),this);
         card.charge(distance*0.5);
       return true;
+    }
+
+    int getDistance(Stop finalStop){
+        //TODO: Add a recursive method to get the distance between initial and final Stations.
+        return 0;
     }
 }
