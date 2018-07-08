fb56e5ea799cf16f95ed136dab3afe599f8b32b3
Richard Gan <richard.gan@mail.utoronto.ca>
2018-07-08 00:25:39 -0400

    TRAN-24 TRAN-25 TRAN-26 TRAN-27 TRAN-28 TRAN-29 TRAN-30 Created initial skeletons for processing of the configuration files.

--- phase1/src/main/TransitManager.java
+++ phase1/src/main/TransitManager.java
@@ -1,4 +1,10 @@
 package main;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 import user.Admin;
 
 /* This class will perform initial setup upon launch. */
@@ -7,7 +13,54 @@ public class TransitManager {
 	private final Admin admin = new Admin();
 
   public static void main(String[] args) {
-	  
+
+      try {
+
+          // Stops file processing
+          BufferedReader br = new BufferedReader(new FileReader("stops.txt"));
+          String fileRead = br.readLine();
+
+          // stopId name
+          while (fileRead != null) {
+              String[] tokenize = fileRead.split(" ");
+          }
+
+          br.close();
+
+          // Stations file processing
+          br = new BufferedReader(new FileReader("stations.txt"));
+          fileRead = br.readLine();
+
+          // stationId name
+          // stationId name connect stopId
+          while (fileRead != null) {
+              String[] tokenize = fileRead.split(" ");
+          }
+
+          br.close();
+
+          // Events file processing
+          br = new BufferedReader(new FileReader("events.txt"));
+          fileRead = br.readLine();
+
+          // startup DATE
+          if (!fileRead.startsWith("startup")) throw new RuntimeException("No date provided on startup");
+          String date = fileRead.split(" ")[1];
+          BufferedWriter bw = new BufferedWriter(new FileWriter(date + ".txt"));
+
+          // tapOn
+          // tapOff
+          // load
+          // suspend
+          while (fileRead != null) {
+              String[] tokenize = fileRead.split(" ");
+          }
+
+      } catch (Throwable e) {
+          e.printStackTrace();
+          System.exit(1);
+      }
+
   }
 
 }
