4166ecd1d4a2d0a0736bce0dad872ebd429c4b6e
Richard Gan <richard.gan@mail.utoronto.ca>
2018-07-11 19:16:33 -0400

    Clarity updates to Trip

--- phase1/src/transitNetwork/Trip.java
+++ phase1/src/transitNetwork/Trip.java
@@ -3,9 +3,10 @@ package transitNetwork;
 import java.util.ArrayList;
 
 public class Trip {
-    private ArrayList<TripLocation> stops;
+    private ArrayList<TripLocation> locations;
     private Station lastSubwayTap = null;
-    
+
+    private int distanceTravelled;
     private boolean tripEnded;
     
     private long initialTime;
@@ -14,19 +15,21 @@ public class Trip {
     private double trueValue = 0;
 
     public Trip(long timestamp, BusStop busStop, Route route) {
-        stops = new ArrayList<>();
-        addStop(timestamp, true, busStop, route);
+        locations = new ArrayList<>();
+        addLocation(timestamp, true, busStop, route, false);
         initialTime = timestamp;
         tripEnded = false;
         tripValue = 0;
+        distanceTravelled = 0;
     }
 
     public Trip(long timestamp, Station initialStation) {
-      stops = new ArrayList<>();
-      addStop(timestamp, true, initialStation);
+      locations = new ArrayList<>();
+      addLocation(timestamp, true, initialStation, false);
       initialTime = timestamp;
         tripEnded = false;
         tripValue = 0;
+        distanceTravelled = 0;
     }
     
     public void charge(double amount) {
@@ -50,7 +53,23 @@ public class Trip {
     }
 
     public Stop getInitialStop(){
-        return stops.get(0).getStop();
+        return locations.get(0).getStop();
+    }
+
+    public Stop getLastStop() {
+      return getLastLocation().getStop();
+    }
+
+    public Route getLastRoute() {
+        return getLastLocation().getRoute();
+    }
+
+    public boolean lastWasTapOn() {
+        return getLastLocation().isTapOn();
+    }
+
+    public TripLocation getLastLocation() {
+        return locations.get(locations.size() - 1);
     }
 
     public double getValue() {
@@ -60,26 +79,45 @@ public class Trip {
     public double getTrueValue() {
         return trueValue;
     }
-    
-    public Stop getLastStop() {
-      return stops.get(stops.size() - 1).getStop();
-    }
-    
+
     public Station getLastSubwayTap() {
       return lastSubwayTap;
     }
     
-    public void addStop(long timestamp, boolean tapType, BusStop stop, Route route) {
-      stops.add(new TripLocation(timestamp, tapType, stop, route));
+    public void addLocation(long timestamp, boolean tappingOn, BusStop stop, Route route, boolean fined) {
+      if (!tappingOn && !fined) {
+          distanceTravelled += route.getDistance((BusStop) getLastStop(), stop);
+      }
+      locations.add(new TripLocation(timestamp, tappingOn, stop, route));
     }
 
-    public void addStop(long timestamp, boolean tapType, Station station) {
-        stops.add(new TripLocation(timestamp, tapType, station));
+    public void addLocation(long timestamp, boolean tappingOn, Station station, boolean fined) {
+        if (!tappingOn && !fined) {
+            distanceTravelled += station.getDistance((Station) getLastStop());
+        }
+        locations.add(new TripLocation(timestamp, tappingOn, station));
         lastSubwayTap = station;
     }
 
     public String toString() {
         StringBuilder details = new StringBuilder();
+        details.append("Trip Details\n");
+        details.append("==================\n");
+        details.append("Started at:  ").append(initialTime);
+        details.append("\nActive:      ").append(!tripEnded);
+        details.append(String.format("\nCost:        $%.2f", tripValue));
+        details.append("\n\nLocation History:\n==================\n");
+        for (TripLocation location : locations) {
+            details.append(String.format("[%d] %s ",
+                    location.getTimestamp(), (location.isTapOn()) ? "tap on " : "tap off "));
+            if (location.isBusStop()) {
+                details.append(String.format("on bus at stop %s on route %s",
+                        location.getStop().getName(), location.getRoute().getId()));
+            }
+            else if (location.isStation()) {
+                details.append("at station ").append(location.getStop().getName());
+            }
+        }
         return details.toString();
     }
 }
