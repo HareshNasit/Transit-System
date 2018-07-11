123aaadae46e1db31fe6886bfa828833a4e3d187
Josh Gelua <josh.gelua@mail.utoronto.ca>
2018-07-11 01:30:11 -0400

    Added JavaDoc for BusStop.java, Route.java, and Station.java

--- phase1/src/transitNetwork/BusStop.java
+++ phase1/src/transitNetwork/BusStop.java
@@ -2,14 +2,37 @@ package transitNetwork;
 
 import main.Logger;
 import user.Card;
-
+/**
+ * @author
+ * @version 1.0
+ */
 public class BusStop extends Stop{
-
+    /**
+     * The constructor of the BusStop that initializes the unique id and the name of the BusStop.
+     * The id must be unique to all Stops, including BusStops and Stations, but the name may be
+     * shared.
+     *
+     * @param id    the identification of the stop that is unique that cannot be changed
+     * @param name  the name of the bus stop
+     * @see         Stop
+     */
     BusStop(String id, String name){
         super(id, name);
     }
 
     //Return true if successful, false otherwise.
+    /**
+     * Returns true if the tap on function of the BusStop was successful by the card.
+     * The tap on function for BusStop charges the card of the user $2.00 when successful, and
+     * the user may travel at any distance so long as they do not tap off.
+     * If the card has less than $0 in its balance, then will tap unsuccessfully.
+     * In both cases, timestamp is recorded.
+     *
+     * @param route     the route that the user is taking at this BusStop location
+     * @param card      the card that is being used to tap onto the BusStop
+     * @param timestamp the time recorded  at which the card taps onto the BusStop
+     * @return          true if successful, false otherwise
+     */
     public boolean tapOn(Route route, Card card, int timestamp) {
         //Test for conditions to start a new trip
         Trip trip = card.getCurrentTrip();
@@ -28,7 +51,18 @@ public class BusStop extends Stop{
         Logger.log(card.toString() + " failed to tap on to bus stop " + getName() + " at timestamp " + timestamp);
         return false;
     }
-
+    /**
+     * Returns true if the tap off function of the BusStop was successful by the card.
+     * The tap off function will not charge the card if it was a legal trip, i.e. the card tapped on
+     * at a BusStop that is legally connected to this BusStop, but if the trip was illegal, i.e.
+     * last tap on was a station or an non-connected BusStop, they will be charged the maximum
+     * of $6.00.
+     *
+     * @param route     the route that the user was taking before tapping off the BusStop
+     * @param card      the card that is being used to tap off the BusStop
+     * @param timestamp the time recorded at which the card taps off the BusStop
+     * @return          true if successful tap off or if user illegally exited the transit without tapping
+     */
     public boolean tapOff(Route route, Card card, int timestamp) {
         //TODO: Add detection for disjointed trips
         Logger.log(card.toString() + " tapped off of bus stop " + getName() + " at timestamp " + timestamp);
@@ -42,18 +76,32 @@ public class BusStop extends Stop{
         }
         return true;
     }
-
+    /**
+     * Connects the stop to this BusStop so that tap on/tap off functions will consider trips between
+     * these two stops legal. Override function.
+     *
+     *  @param stop  connects the stop to this BusStop if station, throws exception otherwise
+     */
     @Override
     void connectStop(Stop stop) {
         if (!(stop instanceof Station)) throw new RuntimeException("BusStop connected with BusStop instead of Station");
         Station station = (Station) stop;
         super.connectStop(station);
     }
-
+    /**
+     * Connects the station to this BusStop so that tap on/tap off functions will consider trips between
+     * the station and the BusStop legal.
+     *
+     * @param station   connects station to this BusStop
+     */
     void connectStation(Station station) {
         connectStop(station);
     }
-
+    /**
+     * Returns the Station that this BusStop is connected to.
+     *
+     * @return      the station that this BusStop is connected to
+     */
     Station getConnectedStation() {
         return (Station) this.getConnectedStop();
     }
--- phase1/src/transitNetwork/Station.java
+++ phase1/src/transitNetwork/Station.java
@@ -5,34 +5,63 @@ import java.util.Queue;
 import main.Logger;
 import java.util.ArrayList;
 import java.util.LinkedList;
-
+/**
+ * @author
+ * @version 1.0
+ */
 public class Station extends Stop{
 
     private ArrayList<Station> connectingStations;
     private static int stationCount;
-
+  /**
+   * The constructor of the Station that initializes the unique id and the name of the Station.
+   * The id must be unique to all Stops, including BusStops and Stations, but the name may be
+   * shared.
+   *
+   * @param id            the identification of the stop that is unique that cannot be changed
+   * @param name          the name of the station
+   * @param stationCount  the total number of stations in the transit network
+   * @see                 Stop
+   */
     public Station(String id, String name, int stationCount){
         super(id, name);
         this.connectingStations = new ArrayList<>();
         this.stationCount = stationCount;
     }
-
-    /* Create a station with a pre-connected bus stop */
+  /**
+   * The constructor of the Station that initializes the unique id and the name of the Station.
+   * The id must be unique to all Stops, including BusStops and Stations, but the name may be
+   * shared. Takes in a BusStop that connects to this Station.
+   *
+   * @param id                  the identification of the stop that is unique that cannot be changed
+   * @param name                the name of the station
+   * @param connectingBusStop   the BusStop that this Station is connected to
+   * @see                       Stop
+   */
     public Station(String id, String name, BusStop connectingBusStop){
         super(id, name);
         this.connectingStations = new ArrayList<>();
         this.connectStop(connectingBusStop);
         connectingBusStop.connectStation(this);
     }
-    
-    /* Connect a single station to this one */
+  /**
+   * Connects an immediate station to this Station.
+   *
+   *
+   *  @param station  connects a station to this Station
+   */
     public void connectStation(Station station) {
       connectingStations.add(station);
       if (!station.getConnectedStations().contains(this)) {
         station.connectStation(this);
       }
     }
-    
+  /**
+   * Connects a list of stations to this station.
+   *
+   * @param stations an arraylist of stations to connect this one to
+   *
+   */
     /* Connect an arrayList of stations to this station */
     public void connectStations(ArrayList<Station> stations) {
       connectingStations.addAll(stations);
@@ -42,7 +71,12 @@ public class Station extends Stop{
         }
       }
     }
-
+  /**
+   * Connects a BusStop to this Station so that they can be used for jointed trips, throws exception
+   * otherwise.
+   *
+   * @param stop    a stop that connects to this Station
+   */
     @Override
     void connectStop(Stop stop) {
         if (!(stop instanceof BusStop)) throw new RuntimeException("Station connected with Station when supposed to be BusStop");
@@ -50,19 +84,41 @@ public class Station extends Stop{
         super.connectStop(busStop);
         busStop.connectStop(this);
     }
-
+  /**
+   * Connects a BusStop to this Station so that they can be used for jointed trips.
+   *
+   * @param busStop    a BusStop that connects to this Station
+   */
     void connectBusStop(BusStop busStop) {
         connectStop(busStop);
     }
-
+  /**
+   * Returns a BusStop that this Station is connected to.
+   * @return  a BusStop
+   *
+   */
     BusStop getConnectedBusStop() {
         return (BusStop) this.getConnectedStop();
     }
-
+  /**
+   * Returns a list of Stations that are connected to this station.
+   * @return a list of Stations
+   */
     public ArrayList<Station> getConnectedStations() {
       return connectingStations;
     }
-
+  /**
+   * Returns true if the tap on function of the Station was successful by the card.
+   * Starts the trip if trip was not already active, otherwise continues trip.
+   *
+   * If card balance is less than or equal to $0.00, then the tap on function returns false and
+   * reports that the tap on has failed.
+   *
+   * @param route     the route that the user is taking at this Station location
+   * @param card      the card that is being used to tap onto the Station
+   * @param timestamp the time recorded  at which the card taps onto the Station
+   * @return          true if successful, false otherwise
+   */
     public boolean tapOn(Route route, Card card, int timestamp) {
       if (card.getBalance() > 0) {
           Stop lastStop = card.getCurrentTrip().getLastStop();
@@ -81,7 +137,17 @@ public class Station extends Stop{
       Logger.log(card.toString() + " failed to tap on to subway station " + getName() + " at timestamp " + timestamp);
       return false;
     }
-    
+  /**
+   * Returns true if the tap off function of the Station was successful by the card.
+   * The tap off function will charge the card if it was a legal trip by the shortest distance
+   * between the initial Station at tap on and this Station. If the card was illegally entered and
+   * taps off, then the system will charge the maximum $6.00.
+   *
+   * @param route     the route that the user was taking before tapping off the Station
+   * @param card      the card that is being used to tap off the Station
+   * @param timestamp the time recorded at which the card taps off the Station
+   * @return          true if successful tap off or if user illegally exited the transit without tapping
+   */
     public boolean tapOff(Route route, Card card, int timestamp) {
         // If the user had entered illegally and jst taps off without tapping on, the user is charged
         // max $6.
@@ -105,7 +171,14 @@ public class Station extends Stop{
         }
       return true;
     }
-
+  /**
+   * Returns an integer that is the shortest distance between the initial Station and the last
+   * Station at tap off.
+   *
+   * @param lastStop the Station that was initially tapped onto
+   * @return         an integer of the distance between initial Station and this Station
+   *
+   */
     /* Breadth-first search
      * null values in the queue separate depth values
      * max depth is the number of existing stations
