2241bf39ed82badd1a991a1acf09cee46cb5341b
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-07-18 13:58:49 -0400

    Added constructors to the startup screens

--- phase2/src/hotcupsofjava/transitsystemmanager/AdminScreen/AdminStartScreen.java
+++ phase2/src/hotcupsofjava/transitsystemmanager/AdminScreen/AdminStartScreen.java
@@ -1,4 +1,12 @@
 package hotcupsofjava.transitsystemmanager.AdminScreen;
 
+import javafx.fxml.FXMLLoader;
+
 public class AdminStartScreen {
+
+    public AdminStartScreen(){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AdStartScreen.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+    }
 }
--- phase2/src/hotcupsofjava/transitsystemmanager/UserScreen/UserStartScreen.java
+++ phase2/src/hotcupsofjava/transitsystemmanager/UserScreen/UserStartScreen.java
@@ -1,4 +1,12 @@
 package hotcupsofjava.transitsystemmanager.UserScreen;
 
+import javafx.fxml.FXMLLoader;
+
 public class UserStartScreen {
+
+    public UserStartScreen(){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UsStartScreen.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+    }
 }
