873d6429f96864e1e3833caf0528b648939bb858
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-07-23 18:52:18 -0400

    Implemented code when edit name is clicked.

--- phase2/src/hotcupsofjava/transitsystemmanager/screens/UserScreen/UserStartScreen.java
+++ phase2/src/hotcupsofjava/transitsystemmanager/screens/UserScreen/UserStartScreen.java
@@ -4,12 +4,16 @@ import hotcupsofjava.transitsystemmanager.objects.userobjects.Card;
 import hotcupsofjava.transitsystemmanager.objects.userobjects.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+
+import java.util.Optional;
 
 public class UserStartScreen {
 
@@ -42,5 +46,16 @@ public class UserStartScreen {
     private void setCardTable(){
         ObservableList<Card> cards = FXCollections.observableArrayList();
         cards.addAll(user.getCards());
+    }
+
+    public void changeName(ActionEvent actionEvent) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Edit Name");
+        dialog.setContentText("New Name");
+        Optional<String> result = dialog.showAndWait();
+        if(result.isPresent()){
+            user.setName(result.get());
+            nameText.setText(result.get());
+        }
     }
 }
