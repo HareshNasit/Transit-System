4ebab08dfe986726b3fe5fb1bfc2690084682c5f
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-07-24 14:40:18 -0400

    Initialized the ids for buttons, tables, tabs and texts in the card screen.

--- phase2/src/hotcupsofjava/transitsystemmanager/screens/UserScreen/CardScreen.java
+++ phase2/src/hotcupsofjava/transitsystemmanager/screens/UserScreen/CardScreen.java
@@ -1,13 +1,37 @@
 package hotcupsofjava.transitsystemmanager.screens.UserScreen;
 
+import hotcupsofjava.transitsystemmanager.managers.RouteManager;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CardScreen implements Initializable {
+
+    public Text cardName;
+    public Text cardID;
+    public Text tab1Balance;
+    public Button suspendCardBtn;
+    public Button removeCardBtn;
+    public TableView subwayTable;
+    public TableColumn subwayNameCol;
+    public TextField tab3BalenceInput;
+    public Text tab3Balance;
+    public Tab tab3;
+    public Button tapOffBtn;
+    public Button tapOnBtn;
+    public TableColumn subwayLineCol;
+    public TableView busStopTable;
+    public TableColumn BusStopNameCol;
+    public TableColumn BusRouteCol;
+    public Tab tab2;
+    public Tab tab1;
+    private RouteManager routeManager;
+
 
 //    public CardScreen(){
 //        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UserStartScreen.fxml"));
