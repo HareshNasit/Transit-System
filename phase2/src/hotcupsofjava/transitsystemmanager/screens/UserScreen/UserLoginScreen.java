ef68e978050f3493d0752a0276b5cd66b9a0d6f2
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-07-28 17:01:24 -0400

    UserLoginScreen now works by creating a new controller for it.

--- phase2/src/hotcupsofjava/transitsystemmanager/screens/UserScreen/UserLoginScreen.java
+++ phase2/src/hotcupsofjava/transitsystemmanager/screens/UserScreen/UserLoginScreen.java
@@ -2,22 +2,38 @@ package hotcupsofjava.transitsystemmanager.screens.UserScreen;
 
 import hotcupsofjava.transitsystemmanager.managers.UserManager;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class UserLoginScreen implements Initializable {
+public class UserLoginScreen extends AnchorPane {
     public Button loginBtn;
     public Label resultLabel;
     public TextField emailText;
     public TextField nameText;
     public UserManager userManager;
 
+    public UserLoginScreen() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UserLoginScreen.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
     public void checkLogin(ActionEvent actionEvent) {
         String name = nameText.getText();
         String email = emailText.getText();
@@ -29,10 +45,5 @@ public class UserLoginScreen implements Initializable {
             resultLabel.setText("Wrong Credentials");
             resultLabel.setTextFill(Color.valueOf("Red"));
         }
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
     }
 }
